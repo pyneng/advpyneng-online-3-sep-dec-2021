c7eab82b7a3c7568b028280f4bda7c2928b82406
pyneng <pyneng.course@gmail.com>
2021-09-25 06:49:13 +0000

    cleaned pytest examples

--- examples/01_pytest_basics/check_ip_functions.py
+++ examples/01_pytest_basics/check_ip_functions.py
@@ -9,15 +9,6 @@ def check_ip(ip):
         return False
 
 
-def test_check_ip():
-    assert (
-        check_ip("10.1.1.1") == True
-    ), "При правильном IP, функция должна возвращать True"
-    assert (
-        check_ip("500.1.1.1") == False
-    ), "Если адрес неправильный, функция должна возвращать False"
-
-
 if __name__ == "__main__":
     result = check_ip("10.1.1.1")
     print("Function result:", result)
