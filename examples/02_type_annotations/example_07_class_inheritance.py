84d0d7c2687291e30bb76557e2099d51ad61d28a
pyneng <pyneng.course@gmail.com>
2021-10-02 13:27:39 +0000

    type annotations examples

--- examples/02_type_annotations/example_07_class_inheritance.py
+++ examples/02_type_annotations/example_07_class_inheritance.py
@@ -1,13 +1,16 @@
+import paramiko
 import time
+from typing import Union, List
 
 
 class BaseSSH:
-    def __init__(self, ip, username, password):
+    def __init__(self, ip: str, username: str, password: str) -> None:
         self.ip = ip
         self.username = username
         self.password = password
+        ####
 
-    def send_config_commands(self, commands):
+    def send_config_commands(self, commands: Union[str, List[str]]) -> str:
         if isinstance(commands, str):
             commands = [commands]
         for command in commands:
@@ -16,9 +19,16 @@ class BaseSSH:
 
 
 class CiscoSSH(BaseSSH):
-    def __init__(self, ip, username, password, secret, disable_paging=True):
+    def __init__(
+        self,
+        ip: str,
+        username: str,
+        password: str,
+        secret: str,
+        disable_paging: bool = True,
+    ) -> None:
         super().__init__(ip, username, password)
 
-    def send_config_commands(self, commands):
+    def send_config_commands(self, commands: Union[str, List[str]]) -> str:
         # send....(conf t)
         return "result"
