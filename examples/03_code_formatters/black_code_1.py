b6a9bb44bc976fcd463012937e2de0b33b2eac65
pyneng <pyneng.course@gmail.com>
2021-10-03 09:00:57 +0000

    black code

--- examples/03_code_formatters/black_code_1.py
+++ examples/03_code_formatters/black_code_1.py
@@ -1,13 +1,26 @@
-from seven_dwwarfs import Grumpy, Happy, Sleepy, Bashful, Sneezy, Dopey, Doc, Section, TestLine
-x = { 'a':37,'b':42, 'c':927}
+from seven_dwwarfs import (
+    Grumpy,
+    Happy,
+    Sleepy,
+    Bashful,
+    Sneezy,
+    Dopey,
+    Doc,
+    Section,
+    TestLine,
+)
 
-x = 123456789.123456789E123456789
+x = {"a": 37, "b": 42, "c": 927}
 
-if very_long_variable_name is not None and \
- very_long_variable_name.field > 0 or \
- very_long_variable_name.is_debug:
- z = 'hello '+'world'
+x = 123456789.123456789e123456789
+
+if (
+    very_long_variable_name is not None
+    and very_long_variable_name.field > 0
+    or very_long_variable_name.is_debug
+):
+    z = "hello " + "world"
 else:
- world = 'world'
- a = 'hello {}'.format(world)
- f = rf'hello {world}'
+    world = "world"
+    a = "hello {}".format(world)
+    f = rf"hello {world}"
