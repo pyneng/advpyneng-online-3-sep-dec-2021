e79cc8c5fe2908e1fd0e017a00480e0120d2d5fd
pyneng <pyneng.course@gmail.com>
2021-10-15 10:08:08 +0000

    logging example update

--- examples/05_logging/logging_api_example_9_filter.py
+++ examples/05_logging/logging_api_example_9_filter.py
@@ -1,20 +1,18 @@
 import logging
 
-
-class DebugOnlyLogging(logging.Filter):
-    def __init__(self, level):
-        self.level = level
-
-    def filter(self, record):
-        return record.levelno == self.level
+from logging_filters import LevelFilter, MessageFilter
 
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
+logger.addFilter(LevelFilter(logging.DEBUG))
+logger.addFilter(MessageFilter("test"))
 
 ### stderr
 console = logging.StreamHandler()
 console.setLevel(logging.DEBUG)
+# console.addFilter(LevelFilter(logging.DEBUG))
+# console.addFilter(MessageFilter("test"))
 formatter = logging.Formatter(
     "{asctime} - {name} - {levelname} - {message}", datefmt="%H:%M:%S", style="{"
 )
@@ -25,7 +23,6 @@ logger.addHandler(console)
 ### File
 logfile = logging.FileHandler("logfile3.log")
 logfile.setLevel(logging.DEBUG)
-# logfile.addFilter(DebugOnlyLogging(logging.DEBUG))
 
 formatter = logging.Formatter("{asctime} - {name} - {levelname} - {message}", style="{")
 logfile.setFormatter(formatter)
@@ -34,5 +31,7 @@ logger.addHandler(logfile)
 
 ## messages
 logger.debug("Сообщение уровня debug")
+logger.debug("Сообщение уровня debug. test1")
+logger.debug("Сообщение уровня debug. test2")
 logger.info("Сообщение уровня info")
 logger.warning("Сообщение уровня warning")
