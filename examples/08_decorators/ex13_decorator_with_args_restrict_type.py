1d749252415204543ebffd1fb99f07cfe5b9a37e
pyneng <pyneng.course@gmail.com>
2021-10-23 11:04:17 +0000

    decorator factory examples

--- examples/08_decorators/ex13_decorator_with_args_restrict_type.py
+++ examples/08_decorators/ex13_decorator_with_args_restrict_type.py
@@ -31,4 +31,29 @@ def arg_type(required_type):
 all_str = arg_type(str)
 all_int = arg_type(int)
 
-f = all_str(f)
+# f = all_str(f)
+
+
+
+def arg_type2(required_type, func):
+    print(f"Создаем декоратор с типом {required_type}")
+    print("Вызываю декоратор")
+    @wraps(func)
+    def inner(*args):
+        if not all(
+            isinstance(arg, required_type) for arg in args
+        ):
+            raise ValueError(
+                f"Все аргументы должны быть {required_type.__name__}"
+            )
+        return func(*args)
+    return inner
+
+def join(*args):
+    return ",".join(args)
+
+def f():
+    pass
+
+join = arg_type2(str, join)
+
