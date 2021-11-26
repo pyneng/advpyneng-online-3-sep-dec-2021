f50e23ea8873c90b3894a6e0890bb66796c1adfa
pyneng <pyneng.course@gmail.com>
2021-11-26 12:24:39 +0000

    sockets examples

--- examples/16_asyncio_basics/socket_examples/ex04_socket_selectors.py
+++ examples/16_asyncio_basics/socket_examples/ex04_socket_selectors.py
@@ -1,13 +1,12 @@
 import socket
-import time
+import selectors
 
 
 def accept(server_socket):
     client_socket, client_address = server_socket.accept()
-    client_socket.setblocking(False) # non-blocking
     print(f"{client_socket=}")
     print(f"{client_address=}")
-    client_sockets_list.append(client_socket)
+    selector.register(client_socket, selectors.EVENT_READ)
 
 
 def read(client_socket):
@@ -15,27 +14,27 @@ def read(client_socket):
     print(f'{data=}')
     client_socket.send(data.upper())
     if b"close" in data:
+        selector.unregister(client_socket)
         client_socket.close()
-        client_sockets_list.remove(client_socket)
 
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-server_socket.setblocking(False) # non-blocking
 server_socket.bind(("localhost", 9000))
 server_socket.listen()
-client_sockets_list = []
+
+selector = selectors.DefaultSelector()
+print(f"{selector=}")
+selector.register(server_socket, selectors.EVENT_READ)
 
 
 while True:
-    try:
-        accept(server_socket)
-    except BlockingIOError:
-        pass
-    for cl_socket in client_sockets_list:
-        try:
-            read(cl_socket)
-        except BlockingIOError:
-            pass
-    time.sleep(1)
-    print("Waiting...")
+    print("Waiting for event...")
+    new_events = selector.select() # selector.select(timeout=3)
+    for event, _ in new_events:
+        event_socket = event.fileobj
+
+        if event_socket is server_socket:
+            accept(server_socket)
+        else:
+            read(event_socket)
