3bdb4ee3f55df723a3e8e87104fbd8486b1fefad
pyneng <pyneng.course@gmail.com>
2021-11-26 12:02:58 +0000

    sockets examples

---
+++ examples/16_asyncio_basics/socket_examples/ex03_socket_non_blocking.py
@@ -0,0 +1,42 @@
+import socket
+import selectors
+
+
+def accept(server_socket):
+    client_socket, client_address = server_socket.accept()
+    client_socket.setblocking(False) # non-blocking
+    print(f"{client_socket=}")
+    print(f"{client_address=}")
+    selector.register(client_socket, selectors.EVENT_READ)
+
+
+def read(client_socket):
+    data = client_socket.recv(4096)
+    print(f'{data=}')
+    client_socket.send(data.upper())
+    if b"close" in data:
+        selector.unregister(client_socket)
+        client_socket.close()
+
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server_socket.setblocking(False) # non-blocking
+server_socket.bind(("localhost", 9000))
+server_socket.listen()
+
+selector = selectors.DefaultSelector()
+print(f"{selector=}")
+selector.register(server_socket, selectors.EVENT_READ)
+
+
+while True:
+    print("Waiting for event...")
+    new_events = selector.select() # selector.select(timeout=3)
+    for event, _ in new_events:
+        event_socket = event.fileobj
+
+        if event_socket is server_socket:
+            accept(server_socket)
+        else:
+            read(event_socket)
