d74d8ebef240aa46ba6887551421fef55eb8a8c1
pyneng <pyneng.course@gmail.com>
2021-11-27 16:47:19 +0000

    lecture 20

--- examples/17_async_libraries/ex51_scrapli_basic.py
+++ examples/17_async_libraries/ex51_scrapli_basic.py
@@ -1,22 +1,27 @@
 import asyncio
-from scrapli.driver.core import AsyncIOSXEDriver
+from scrapli import AsyncScrapli
+from devices_scrapli import devices
+
 
 r1 = {
-   "host": "192.168.100.1",
-   "auth_username": "cisco",
-   "auth_password": "cisco",
-   "auth_secondary": "cisco",
-   "auth_strict_key": False,
-   "transport": "asyncssh",
+    "host": "192.168.100.1",
+    "auth_username": "cisco",
+    "auth_password": "cisco",
+    "auth_secondary": "cisco",
+    "auth_strict_key": False,
+    "transport": "asyncssh",
+    "platform": "cisco_iosxe",
 }
 
+
 async def send_show(device, command):
-    async with AsyncIOSXEDriver(**device) as conn:
+    async with AsyncScrapli(**device) as conn:
         result = await conn.send_command(command)
     return result.result
 
 
 if __name__ == "__main__":
+    # r1 = devices[0]
     output = asyncio.run(send_show(r1, "show ip int br"))
     print(output)
 
