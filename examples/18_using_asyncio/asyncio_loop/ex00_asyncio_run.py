740e430f53418c0760201e5f5978d76e8fe08123
pyneng <pyneng.course@gmail.com>
2021-12-10 19:32:33 +0000

    added examples

--- examples/18_using_asyncio/asyncio_loop/ex00_asyncio_run.py
+++ examples/18_using_asyncio/asyncio_loop/ex00_asyncio_run.py
@@ -10,10 +10,13 @@ async def send_show(device, command):
     try:
         async with AsyncScrapli(**device) as conn:
             reply = await conn.send_command(command)
-            # await asyncio.sleep(3)
+            await asyncio.sleep(10)
         return reply.result
     except ScrapliException as error:
         print(error)
+    except asyncio.CancelledError:
+        print("Task is cancelled")
+        raise
 
 
 async def run_all(devices, command):
